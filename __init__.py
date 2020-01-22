fff6a00446c68f049b6484c33992c9d25557798a
HimanshuMittal01 <greathimanshumittal@gmial.com>
2020-01-23 01:18:49 +0530

    added intent essentials, reviews

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from tour_assistant.root import app
 
 import tour_assistant.greeting
+import tour_assistant.tourism
+import tour_assistant.unknown
 
 __all__ = ['app']
--- tourism.py
+++ tourism.py
@@ -1,3 +1,73 @@
 from .root import app
 
-#Here we have to make changes for toursism intents
+#TODO: Optimize the qa class searching
+
+@app.handle(intent="find_essentials")
+def find_essentials(request, responder):
+    #See is user already taked about the location
+    selected_center_name = request.frame.get('center_name')
+
+    #TODO: Write the logic for location as well
+
+    if selected_location:
+        #If a location has been selected, store the visiting location in chatbot
+        responder.slots['center_name'] = selected_center_name
+
+        #TODO: Write the backend code to find out weather of the location
+
+        latitide = None
+        longitude = None
+        weather = None
+        #Get the coordinates
+        for c in app.question_answerer.get('centers'):
+            if c['name']==selected_center_name:
+                latitude = c['location'][1]['latitide']
+                longitude = c['location'][1]['longitude']
+
+                #TODO: Write the backend code to find out weather of the location
+
+        if latitude!=None and longitude!=None:
+            #For now, I am assuming that weather is cold and taking general items into account
+
+            #TODO: We can ask for duration if we want to specify the quantity of the essentials
+            replies = ['It is recommended to take warm clothes, bed sheet and few walnuts.']
+        else:
+            replies = [f'I am not able to find the location of {selected_center_name}. Could you please specify it?']
+            
+    else:
+        #If no location has been specified, then prompt the user to tell location
+        replies = ["I'm not sure. You haven't told me where you want to go!"]
+
+    responder.reply(replies)
+
+
+@app.handle(intent="list_reviews")
+def list_reviews(request, responder):
+    #See is user already taked about the location
+    selected_center_name = request.frame.get('center_name')
+
+    if selected_location:
+        #If a location has been selected, store the visiting location in chatbot
+        responder.slots['center_name'] = selected_center_name
+
+        reviews = None
+        for c in app.question_answerer.get('centers'):
+            if c['name']==selected_center_name:
+                reviews = c['reviews']
+
+        if reviews:
+            replies = "Here are some few reviews:\n"
+            for i in range(len(reviews)):
+                replies += f"{i}. {reviews[i]}\n"
+
+            #TODO: Give them overall score of reviews through text classification models
+            replies = reviews
+        else:
+            #TODO: We can write logic to verify with its opening date and give proper reply back
+            replies = "The center has been started recently. There are no useful reviews yet."
+
+    else:
+        #If no location has been specified, then prompt the user to tell location
+        replies = ["I'm not sure. You haven't told me what to review"]
+
+    responder.reply(replies)
